86caf0030248052ac0aa728cf270be899ac79003
lelugones73 <71748955+lelugones73@users.noreply.github.com>
2021-10-12 06:43:27 -0300

    Initial commit

---
+++ src/main/java/com/bd/airline/model/User.java
@@ -0,0 +1,63 @@
+package com.bd.airline.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Long id;
+
+    @Column(name = "username", nullable = false, length = 20, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 20)
+    private String password;
+
+    @Column(name = "email", nullable = false, length = 50, unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
